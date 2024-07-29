bdce95c670e8c0ea99f97b78a95f90bf527dae45
Telmo Sá <telmo.sa@farfetch.com>
2024-07-29 15:02:18 +0100

    Add project files.

---
+++ BasketDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BasketDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
